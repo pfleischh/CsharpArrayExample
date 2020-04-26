bfb39e4d15e94bd7f2d99667ad5676bf9d3090c9
pfleischh <46428691+pfleischh@users.noreply.github.com>
2020-04-26 10:58:14 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace FlePTopicReviewArrays
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetNewNumbers = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtSmallestNumber = new System.Windows.Forms.TextBox();
+            this.txtCountSmallNumb = new System.Windows.Forms.TextBox();
+            this.txtArrayLocation = new System.Windows.Forms.TextBox();
+            this.txtNumbers = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnGetNewNumbers
+            // 
+            this.btnGetNewNumbers.Location = new System.Drawing.Point(32, 100);
+            this.btnGetNewNumbers.Name = "btnGetNewNumbers";
+            this.btnGetNewNumbers.Size = new System.Drawing.Size(115, 24);
+            this.btnGetNewNumbers.TabIndex = 0;
+            this.btnGetNewNumbers.Text = "Get New Numbers";
+            this.btnGetNewNumbers.UseVisualStyleBackColor = true;
+            this.btnGetNewNumbers.Click += new System.EventHandler(this.btnGetNewNumbers_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(32, 155);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(115, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtSmallestNumber
+            // 
+            this.txtSmallestNumber.Location = new System.Drawing.Point(218, 184);
+            this.txtSmallestNumber.Name = "txtSmallestNumber";
+            this.txtSmallestNumber.ReadOnly = true;
+            this.txtSmallestNumber.Size = new System.Drawing.Size(226, 20);
+            this.txtSmallestNumber.TabIndex = 3;
+            this.txtSmallestNumber.TabStop = false;
+            // 
+            // txtCountSmallNumb
+            // 
+            this.txtCountSmallNumb.Location = new System.Drawing.Point(218, 210);
+            this.txtCountSmallNumb.Name = "txtCountSmallNumb";
+            this.txtCountSmallNumb.ReadOnly = true;
+            this.txtCountSmallNumb.Size = new System.Drawing.Size(226, 20);
+            this.txtCountSmallNumb.TabIndex = 4;
+            this.txtCountSmallNumb.TabStop = false;
+            // 
+            // txtArrayLocation
+            // 
+            this.txtArrayLocation.Location = new System.Drawing.Point(218, 236);
+            this.txtArrayLocation.Name = "txtArrayLocation";
+            this.txtArrayLocation.ReadOnly = true;
+            this.txtArrayLocation.Size = new System.Drawing.Size(226, 20);
+            this.txtArrayLocation.TabIndex = 5;
+            this.txtArrayLocation.TabStop = false;
+            // 
+            // txtNumbers
+            // 
+            this.txtNumbers.Location = new System.Drawing.Point(218, 12);
+            this.txtNumbers.Name = "txtNumbers";
+            this.txtNumbers.ReadOnly = true;
+            this.txtNumbers.Size = new System.Drawing.Size(226, 166);
+            this.txtNumbers.TabIndex = 6;
+            this.txtNumbers.TabStop = false;
+            this.txtNumbers.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 287);
+            this.Controls.Add(this.txtNumbers);
+            this.Controls.Add(this.txtArrayLocation);
+            this.Controls.Add(this.txtCountSmallNumb);
+            this.Controls.Add(this.txtSmallestNumber);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnGetNewNumbers);
+            this.Name = "Form1";
+            this.Text = "9";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetNewNumbers;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtSmallestNumber;
+        private System.Windows.Forms.TextBox txtCountSmallNumb;
+        private System.Windows.Forms.TextBox txtArrayLocation;
+        private System.Windows.Forms.RichTextBox txtNumbers;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/* Author: Peter Fleischhacker
+ * Date: 4/1/19
+ * Topic Review - Arrays
+ */
+
+namespace FlePTopicReviewArrays
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // loads ProcessArray function at form load
+            ProcessArray();
+        }
+
+       
+
+        
+        public void ProcessArray()
+        {
+          //  Random num = new Random();
+            //create 5x5 2d array called numArray
+            int[,] numArray = new int[5, 5];
+       
+            Random num = new Random();
+
+            // int to hold the numbers
+            int tempNum;
+
+            // string to hold the random numbers before putting them in txtNumbers box
+            string temp = "";
+
+            
+            // for loop to set random numbers between 1 and 50 to array
+            for (int x = 0; x < numArray.GetLength(0); x++)
+            {
+                for (int y = 0; y < numArray.GetLength(1); y++)
+                {
+                    tempNum = num.Next(0, 50);
+                    numArray[x, y] = tempNum;
+                    temp += tempNum.ToString() + "\t";
+                }
+                if (x < numArray.GetLength(0) - 1) temp += "\r\n" ;
+
+                // find the smallest number in the array 
+                int smallest = numArray.Cast<int>().Min();
+
+                // display smallest number in txtSmallestNumber text box
+                txtSmallestNumber.Text = "Smallest number is " + smallest.ToString() + ".";
+
+                // display the number of times the smallest number appears in txtCountSmallNumb
+              //  int count;
+
+                    
+              //  txtCountSmallNumb.Text = "Found " + count.ToString() + " time(s) in these coords:";
+
+                // display the coordinates of where the smallest number appears in the array in txtArrayLocation
+               // txtArrayLocation.Text = ;
+            }
+            // display numbers in text box txtNumbers
+            txtNumbers.Text = temp;
+
+
+
+        }
+
+        private void btnGetNewNumbers_Click(object sender, EventArgs e)
+        {
+            // call ProcessArray() to get new numbers
+            ProcessArray();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            // close application
+            this.Close();
+        }
+    }
+}
